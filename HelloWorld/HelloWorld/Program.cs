9b08f96156fac1d82f5e1637af121113e944fb07
PhilleBOI <philip.thilen@elev.ga.ntig.se>
2020-09-07 21:00:40 +0200

    Labb Vilkor och Loopar

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -16,13 +16,65 @@ namespace HelloWorld
             Console.WriteLine(" \nSkriv din ålder. \n");
             var age = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine( "\nDitt namn är: " + name);
+            string beard = "";
+            while(age >= 18){
+                Console.WriteLine(" \nHar du skäg? \n");
+                beard = Console.ReadLine();
+                if (beard == "ja")
+                    break;
+                else if(beard == "nej")
+                    break;
+            }
+
+            var namn = 0;
+
+            if (name == "Philip")
+                namn = 1;
+            else if (name == "Ludvig")
+                namn = 2;
+            else if (name == "Filippa")
+                namn = 3;
+            else if (name == "Adam")
+                namn = 4;
+            else if (name == "Linus")
+                namn = 5;
+
+            switch (namn)
+            {
+                case 1:
+                    Console.WriteLine("\nVäldigt fint namn måste jag säga");
+                    break;
+                case 2:
+                    Console.WriteLine("\nAha han... Wanna be orten pojk...");
+                    break;
+                case 3:
+                    Console.WriteLine("\nDet är ju duuuuuu, Hej fina du <3.");
+                    break;
+                case 4:
+                    Console.WriteLine("\nDet är ju fan mannen myten legenden!!");
+                    break;
+                case 5:
+                    Console.WriteLine("\nLike your cut g!");
+                    break;
+                default:
+                    Console.WriteLine("\nDu är jättefin idag <3.");
+                    break;
+
+            }
+
+            Console.WriteLine( "Ditt namn är: " + name);
+            Console.WriteLine("Du är ");
+            for (int j = 0; j <= age; j++)
+            {
+                Console.WriteLine(j);
+            }
             Console.WriteLine("Din older är: " + age);
+            Console.WriteLine("Skäg: " + beard);
 
             int i = 20;
 
             if (age < i){
-                Console.WriteLine("Du får inte köpa sprit än på systemet haha!");
+                Console.WriteLine("Btw, du får inte köpa sprit på systemet haha!");
             }
             else {
                 Console.WriteLine("Nu får du bli alkolist, grattis hehe!");
---
+++ HelloWorld/Prog2/Arrays.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Arrays
+    {
+        static void Main(string[] args){
+
+            string[] names = new string[5];
+
+            Console.WriteLine();
+
+        }
+
+    }
+}
