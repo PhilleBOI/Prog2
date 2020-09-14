fac74b99f0c22eac927c7d7b3175f03262b25be6
PhilleBOI <philip.thilen@elev.ga.ntig.se>
2020-09-14 14:17:13 +0200

    Arrays

--- HelloWorld/Prog2/Arrays.cs
+++ HelloWorld/Prog2/Arrays.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace HelloWorld
@@ -10,7 +11,19 @@ namespace HelloWorld
 
             string[] names = new string[5];
 
-            Console.WriteLine();
+            Console.WriteLine("Skriv fem namn: ");
+
+            for(int i = 0; i < names.Length; i++)
+            {
+                names[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("whoops, nu skrivs dom baklänges.");
+
+            for (var i = names.Length -1; i >= 0; i--)
+            {
+                Console.WriteLine(names[i]);
+            }
 
         }
 
