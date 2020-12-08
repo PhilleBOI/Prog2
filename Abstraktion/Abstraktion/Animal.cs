f864fc5c3c28c709549040b11797aaf016a08f3b
PhilleBOI <54800654+PhilleBOI@users.noreply.github.com>
2020-12-08 13:15:27 +0100

    12-08

---
+++ Abstraktion/Abstraktion/Animal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstraktion
+{
+    abstract class Animal
+    {
+
+        public int NumberOfLimbs { get; set; }
+
+        public string Color { get; set; }
+
+        public abstract void Breathe();
+
+        public abstract void Eat();
+
+        public abstract void Rest();
+    }
+}
---
+++ Abstraktion/Abstraktion/Shark.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstraktion
+{
+    class Shark : Animal
+    {
+
+        public Shark()
+        {
+            NumberOfLimbs = 5;
+        }
+
+        public override void Breathe()
+        {
+            Console.WriteLine("Shark is sifting water for oxygen!");
+        }
+
+        public override void Eat()
+        {
+            Console.WriteLine("Shark is devouring smaller fish!");
+        }
+
+        public override void Rest()
+        {
+            Console.WriteLine("Shark is put on autopilot.");
+        }
+
+        //Egen funktionalitet.
+        public void Swim()
+        {
+            Console.WriteLine("Shark is swiming!");
+        }
+    }
+}
